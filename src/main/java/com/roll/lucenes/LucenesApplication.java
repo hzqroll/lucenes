486658f3f52dabd09dd48ece2b080661d47f6b69
zongqiang.hao <zongqiang.hao>
2018-10-05 12:19:33 +0800

    commit

---
+++ src/main/java/com/roll/lucenes/LucenesApplication.java
@@ -0,0 +1,12 @@
+package com.roll.lucenes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucenesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LucenesApplication.class, args);
+    }
+}
